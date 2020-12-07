1863e56ba855009ab284b5abf9caf141cadc4dd6
bhoomi221 <48841472+bhoomi221@users.noreply.github.com>
2020-12-06 20:58:49 -0500

    Add files via upload
    score works, make speed up more obvious, notifications are wack, platypus character need one more feature, prolly falling platforms

--- doodlejump.s
+++ doodlejump.s
@@ -1,4 +1,4 @@
-#####################################################################
+  #####################################################################
 #
 # CSCB58 Fall 2020 Assembly Final Project
 # University of Toronto, Scarborough
@@ -14,13 +14,14 @@
 #
 # Which milestone is reached in this submission?
 # (See the assignment handout for descriptions of the milestones)
-# - Milestone 4
+# - Milestone 5
 #
 # Which approved additional features have been implemented?
 # (See the assignment handout for the list of additional features)
 # 1. nicer graphics 
-# 2. dynamic notifcations
-# 3. (fill in the feature, if any)
+# 2. speed up as time goes on ( increase in difficulty
+# 3. dynamic notifications
+# 4. 
 # ... (add more if necessary)
 #
 # Link to video demonstration for final submission:
@@ -40,13 +41,13 @@
 	#Colors
 	backgroundColour: .word	0x7fe5f0 #blue
 	platformColour: .word 0xf7347a #pink
-	doodlerColour: .word 0x8a2be2 #purple
+	#doodlerColour: .word 0x8a2be2 #purple
 	limeGreen: .word 0x00ff00  
-	body: .word 0x19b092 #turquoise
+	doodlerColour: .word 0x19b092 #turquoise
 	black: .word 0x000000
 	orange: .word 0xfa9d00
 	red: .word 0xff0000
-	scoreColour: .word 0x0000ff
+	scoreCol: .word 0x0000ff
 
 	#Doodler
 	doodlerPositionX1: .word 32
@@ -60,15 +61,12 @@
 
   	#Score
   	score: .word 0
-	notifThreshold: .word 1
+  	score2: .word 0
+	notifThreshold: .word 5
 	notifNum: .word 0
 
   	#End
   	gameEnd: .word 0
-
-  	#levels
-  	mode:   .word 0
-  
 
 .text
 main:
@@ -82,15 +80,15 @@ main:
 
 	jal fillBackground
 	jal promptUser
-	pressToStart:
-		lw $t1, 0xFFFF0004		# check to see which key has been pressed
-		beq $t1, 0x00000031, start # 1 pressed
+	#pressToStart:
+		#lw $t1, 0xFFFF0004		# check to see which key has been pressed
+		#beq $t1, 0x00000031, start # 1 pressed
 
 		li $a0, 250	#
 		li $v0, 32	# pause for 250 milisec
 		syscall		#
 
-		j pressToStart    # Jump back to the top of the wait loop
+		#j pressToStart    # Jump back to the top of the wait loop
 
 start:
 	lw $a0, screenWidth
@@ -103,9 +101,10 @@ start:
 
 	jal fillBackground
 	#jal DrawPlatformInitial
-	jal drawDoodlerInitial
+	
 
 	jal createPlatformsEasy 
+	#jal drawDoodlerInitial
 	jal createPlatformsEasy 
 	jal createPlatformsEasy 
 	jal createPlatformsEasy 
@@ -137,15 +136,14 @@ exit:
 # Draw Doodler intially
 ######################################################
 drawDoodlerInitial:
-	lw $a0, doodlerPositionX1
-	lw $a1, doodlerPositionY1
+	la $t0, platformsEasy 
+	lw $a0, 0($t0)
+	
+	addi $a0, $a0, -240
 
 	addi $sp, $sp, -4
 	sw $ra, 0($sp)
-
-	jal coordinateToAddress
-
-	move $a0, $v0
+	
 	lw $a1, doodlerColour
 
 	jal drawDoodler
@@ -168,6 +166,7 @@ createPlatformsEasy:
 	li $a1, 50
 	syscall
 	#store X position
+	addi $a0, $a0, 10
 	sw $a0, platformPositionX
 	lw $a1, 20($t1)
 	
@@ -183,6 +182,8 @@ createPlatformsEasy:
 zero:
 	li $a1, 60
 	sw $a1, platformPositionY
+	sw $a1, doodlerPositionY1
+	sw $a0, doodlerPositionX1
 	
 	jal coordinateToAddress
 	move $a0, $v0
@@ -284,8 +285,7 @@ drawPlatform:
 	sw $ra, 0($sp)
 	#a0 is the address
 	#a1 is the colour
-	#lw $a3, mode
-	#beq $a3, 2, smaller
+
 	sw $a1, 0($a0)
 	sw $a1, 4($a0)
 	sw $a1, 8($a0)
@@ -315,6 +315,7 @@ drawPlatform:
 	sw $a1, 304($a0)
 	sw $a1, 308($a0)
 
+	
 	lw $ra 0($sp)
 	addi $sp, $sp, 4
 
@@ -380,7 +381,7 @@ moveUp:
 	addi $sp, $sp, -4
 	sw $ra, 0($sp)
 
-	li $t2, 14
+	li $t2, 22
 	li $t3, 0
 	WHILE:
 		beq $t2, $t3, return
@@ -424,6 +425,8 @@ moveUp:
 		jal sleep
 
 		jal inputCheck
+		
+		jal drawScore
 
 		j WHILE
 
@@ -468,6 +471,8 @@ moveDown:
 		lw $a1, doodlerColour
 
 		jal drawDoodler
+		
+		jal redrawPlatforms
 
 		jal sleep
 
@@ -489,11 +494,61 @@ moveDown:
 ##################################################################
 drawDoodler:
 
+#legs
 	sw $a1, 0($a0)
-	sw $a1, 4($a0)
+	sw $a1, 16($a0)
+#body
+	sw $a1, -260($a0)
 	sw $a1, -256($a0)
 	sw $a1, -252($a0)
+	sw $a1, -248($a0)
+	sw $a1, -244($a0)
+	sw $a1, -240($a0)
+	sw $a1, -236($a0)
+	sw $a1, -512($a0)
+	sw $a1, -508($a0)
+	sw $a1, -504($a0)
+	sw $a1, -500($a0)
+	sw $a1, -496($a0)
+	sw $a1, -772($a0)
+	sw $a1, -764($a0)
+	sw $a1, -760($a0)
+	sw $a1, -756($a0)
+	sw $a1, -752($a0)
+	sw $a1, -748($a0)
+	sw $a1, -1028($a0)
+	sw $a1, -1024($a0)
+	sw $a1, -1020($a0)
+	sw $a1, -1016($a0)
+	sw $a1, -1012($a0)
+	sw $a1, -1008($a0)
+	sw $a1, -1004($a0)
 
+	lw $a2, backgroundColour
+	beq $a1, $a2, stay
+#bill & tail
+	lw $a1, orange
+	sw $a1, -520($a0)
+	sw $a1, -516($a0)
+	sw $a1, -492($a0)
+	sw $a1, -488($a0)
+	sw $a1, -484($a0)
+#eye
+	lw $a1, black
+	sw $a1, -768($a0)
+	j doneDrawing
+stay:
+#bill & tail
+	sw $a1, -520($a0)
+	sw $a1, -516($a0)
+	sw $a1, -492($a0)
+	sw $a1, -488($a0)
+	sw $a1, -484($a0)
+#eye
+	sw $a1, -768($a0)
+	j doneDrawing
+
+doneDrawing:
 	jr $ra
 
 ##################################################################
@@ -517,10 +572,31 @@ checkEndGame:
 # sleep function
 ##################################################################
 sleep:
+	lw $a0, score 
+	bge $a0, 15, fast
+	bge $a0, 10, normal 
+	bge $a0, 5, slow 
+	
+	li $a0, 70
+	li $v0, 32
+	syscall
+	j exit7
+fast:
+	li $a0, 20
+	li $v0, 32
+	syscall
+	j exit7
+slow:	
+	li $a0, 50
+	li $v0, 32
+	syscall
+	j exit7
+normal:	
 	li $a0, 30
 	li $v0, 32
 	syscall
-
+	j exit7
+exit7:
 	jr $ra
 
 ##################################################################
@@ -607,7 +683,7 @@ left:
 	jal coordinateToAddress
 
 	move $a0, $v0
-	lw $a2, doodlerColour
+	lw $a1, doodlerColour
 
 
 	jal drawDoodler
@@ -762,15 +838,34 @@ doodlerDownEasy:
 
 	j doodlerDownEasy
 create:
-	lw $a0, score
-	addi $a0, $a0, 1
-	sw $a0, score
+	jal scoreCalc
 	jal createPlatformsEasy
 exitEasy:
 	lw $ra 0($sp)
 	addi $sp, $sp, 4
 	jr $ra
-
+##################################################################
+# calculates the score
+##################################################################
+scoreCalc:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	lw $a0, score 
+	lw $a1, score2
+	beq $a1, 9, change 
+	addi $a1, $a1, 1
+	sw $a1, score2
+	j exit8
+change:
+	addi $a0, $a0, 1
+	sw $a0, score 
+	li $a1, 0
+	sw $a1, score2
+exit8:
+ 	lw $ra 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
 ##################################################################
 # tells you you lost
 ##################################################################
@@ -972,47 +1067,6 @@ bye:
 	syscall	
 	
 
-##################################################################
-# Draw score
-#################################################################
-drawScore:
-	addi $sp, $sp, -4
-	sw $ra, 0($sp)
-
-	lw $t0, score
-	li $a0, 1
-	li $a1, 1
-
-	jal coordinateToAddress
-
-	move $a0, $v0
-	sw $a1, platformColour
-	beqz $a0, zer0
-	beq $a0, 1, one1
-	j leave1
-zer0:
-	sw $a1 4($a0)
-	sw $a1 8($a0)
-	sw $a1 256($a0)
-	sw $a1 268($a0)
-	sw $a1 512($a0)
-	sw $a1 520($a0)
-	sw $a1 772($a0)
-	sw $a1 776($a0)
-	
-	j leave1
-one1:
-	sw $a1 0($a0)
-	sw $a1 256($a0)
-	sw $a1 512($a0)
-	sw $a1 768($a0)
-leave1:
-	#jal notifications
-	
-	lw $ra, 0($sp)
-	addi $sp, $sp, 4
-
-	jr $ra
 
 ##################################################################
 # Tell user what to do to start
@@ -1380,264 +1434,765 @@ notifications:
 	addi $sp, $sp, -4
    	sw $ra, 0($sp)
    	
-	lw $s5, score
+	lw $s5, score2
 	lw $s6, notifThreshold
 	bge $s5, $s6, notif
 	j leave4
-	# $a0 the x coordinate
-# $a1 the y starting coordinate
-# $a2 the color
-# $a3 the y ending coordinate
+
 notif:
-	lw $t0, notifNum 
-	beq $t0, 0, cool 
-	beq $t0, 1, notbad 
-	#beq $t0, 2, udontsuck 
-	#beq $t0, 2, pog
+	lw $s7, notifNum 
+	beq $s7, 0, cool 
+	beq $s7, 1, notbad 
+	beq $s7, 2, pog
 cool:
 
 	lw $a2, orange
-	li $a0, 7
+	li $a0, 15
 	li $a1, 2
 	li $a3, 6
 	jal DrawVerticalLine 
 	
-	li $a0, 8
+	li $a0, 16
 	jal DrawPoint
 	
-	li $a0, 11
+	li $a0, 19
 	jal DrawVerticalLine 
-	li $a0, 12
+	li $a0, 20
 	jal DrawPoint	
 	
-	li $a0, 13
-	jal DrawVerticalLine 
-	
-	li $a0, 16
-	jal DrawVerticalLine 
-	li $a0, 17
-	jal DrawPoint
-	
-	li $a0, 18
-	jal DrawVerticalLine 
-
 	li $a0, 21
 	jal DrawVerticalLine 
+	
+	li $a0, 24
+	jal DrawVerticalLine 
+	li $a0, 25
+	jal DrawPoint
+	
+	li $a0, 26
+	jal DrawVerticalLine 
 
-	li $a0, 8
+	li $a0, 29
+	jal DrawVerticalLine 
+
+	li $a0, 16
 	li $a1, 6
 	jal DrawPoint
 	
-	li $a0, 12
+	li $a0, 20
 	li $a1, 6
 	jal DrawPoint
 	
-	li $a0, 17
+	li $a0, 25
 	li $a1, 6
 	jal DrawPoint
 	
-	li $a0, 22
+	li $a0, 30
 	li $a1, 6
 	jal DrawPoint
 	
+####display######	
 	li $v0 32
 	li $a0, 500
 	syscall
-	li $v0 32
-	li $a0, 500
+####display######	
 	
 	lw $a2, backgroundColour
-	li $a0, 7
+	li $a0, 15
 	li $a1, 2
 	li $a3, 6
 	jal DrawVerticalLine 
 	
-	li $a0, 8
+	li $a0, 16
 	jal DrawPoint
 	
-	li $a0, 11
+	li $a0, 19
 	jal DrawVerticalLine 
-	li $a0, 12
+	li $a0, 20
 	jal DrawPoint	
 	
-	li $a0, 13
-	jal DrawVerticalLine 
-	
-	li $a0, 16
-	jal DrawVerticalLine 
-	li $a0, 17
-	jal DrawPoint
-	
-	li $a0, 18
-	jal DrawVerticalLine 
-
 	li $a0, 21
 	jal DrawVerticalLine 
+	
+	li $a0, 24
+	jal DrawVerticalLine 
+	li $a0, 25
+	jal DrawPoint
+	
+	li $a0, 26
+	jal DrawVerticalLine 
 
-	li $a0, 8
+	li $a0, 29
+	jal DrawVerticalLine 
+
+	li $a0, 16
 	li $a1, 6
 	jal DrawPoint
 	
-	li $a0, 12
+	li $a0, 20
 	li $a1, 6
 	jal DrawPoint
 	
-	li $a0, 17
+	li $a0, 25
 	li $a1, 6
 	jal DrawPoint
 	
-	li $a0, 22
+	li $a0, 30
 	li $a1, 6
 	jal DrawPoint
 	
-	addi $t0, $t0, 1
-	sw $t0, notifNum 
+	addi $s7, $s7, 1
+	sw $s7, notifNum 
+	
+	addi $s6, $s6, 5
+	sw $s6, notifThreshold
+	
+	j leave4
 notbad:
 	lw $a2, orange
-	li $a0, 7
+	li $a0, 15
 	li $a1, 2
-	li $a3, 28
+	li $a3, 36
 	jal DrawHorizontalLine 
-	li $a0, 10
-	lw $a2, backgroundColour
-	jal DrawPoint
-	li $a0, 14
-	jal DrawPoint
 	li $a0, 18
+	lw $a2, backgroundColour
 	jal DrawPoint
 	li $a0, 22
 	jal DrawPoint
 	li $a0, 26
 	jal DrawPoint
+	li $a0, 30
+	jal DrawPoint
+	li $a0, 34
+	jal DrawPoint
 	
 	lw $a2, orange
-	li $a0, 7
+	li $a0, 15
 	li $a1, 2
 	li $a3, 6
 	jal DrawVerticalLine 
-	li $a0, 9
-	jal DrawVerticalLine 
-	li $a0, 11
-	jal DrawVerticalLine 
-	li $a0, 13
-	jal DrawVerticalLine 
-	li $a0, 16
+	li $a0, 17
 	jal DrawVerticalLine 
 	li $a0, 19
-	jal DrawVerticalLine
+	jal DrawVerticalLine 
 	li $a0, 21
 	jal DrawVerticalLine 
-	li $a0, 23
+	li $a0, 24
 	jal DrawVerticalLine 
-	li $a0, 25
-	jal DrawVerticalLine  
 	li $a0, 27
+	jal DrawVerticalLine
+	li $a0, 29
+	jal DrawVerticalLine 
+	li $a0, 31
+	jal DrawVerticalLine 
+	li $a0, 33
+	jal DrawVerticalLine  
+	li $a0, 35
 	jal DrawVerticalLine 
 	
 	lw $a2, orange
-	li $a0, 29
+	li $a0, 37
 	li $a1, 3
 	li $a3, 5
 	jal DrawVerticalLine 
 	
 	lw $a2, orange
-	li $a0, 19
+	li $a0, 27
 	li $a1, 4
-	li $a3, 24
+	li $a3, 32
 	jal DrawHorizontalLine
-	li $a0, 22 
+	li $a0, 30
 	lw $a2, backgroundColour
 	jal DrawPoint
+	li $a0, 28
 	li $a1, 6
 	lw $a2, orange
 	jal DrawPoint
 	
-	li $a0, 12
+	li $a0, 20
 	li $a1, 6
 	jal DrawPoint
-	li $a0, 28
+	li $a0, 36
 	jal DrawPoint
 	li $a1, 2
 	jal DrawPoint
-	
+####display######	
 	li $v0 32
 	li $a0, 500
 	syscall
-	li $v0 32
-	li $a0, 500
+####display######	
 	
 	lw $a2, backgroundColour
-	li $a0, 7
+	li $a0, 15
 	li $a1, 2
-	li $a3, 28
+	li $a3, 36
 	jal DrawHorizontalLine 
-	li $a0, 10
-
+	li $a0, 18
+	lw $a2, backgroundColour
+	jal DrawPoint
+	li $a0, 22
+	jal DrawPoint
+	li $a0, 26
+	jal DrawPoint
+	li $a0, 30
+	jal DrawPoint
+	li $a0, 34
+	jal DrawPoint
 	
 	lw $a2, backgroundColour
-	li $a0, 7
+	li $a0, 15
 	li $a1, 2
 	li $a3, 6
 	jal DrawVerticalLine 
-	li $a0, 9
-	jal DrawVerticalLine 
-	li $a0, 11
-	jal DrawVerticalLine 
-	li $a0, 13
-	jal DrawVerticalLine 
-	li $a0, 16
+	li $a0, 17
 	jal DrawVerticalLine 
 	li $a0, 19
-	jal DrawVerticalLine
+	jal DrawVerticalLine 
 	li $a0, 21
 	jal DrawVerticalLine 
-	li $a0, 23
+	li $a0, 24
 	jal DrawVerticalLine 
-	li $a0, 25
-	jal DrawVerticalLine  
 	li $a0, 27
+	jal DrawVerticalLine
+	li $a0, 29
+	jal DrawVerticalLine 
+	li $a0, 31
+	jal DrawVerticalLine 
+	li $a0, 33
+	jal DrawVerticalLine  
+	li $a0, 35
 	jal DrawVerticalLine 
 	
 	lw $a2, backgroundColour
-	li $a0, 29
+	li $a0, 37
 	li $a1, 3
 	li $a3, 5
 	jal DrawVerticalLine 
 	
-	lw $a2, backgroundColour
-	li $a0, 19
+	lw $a2, orange
+	li $a0, 27
 	li $a1, 4
-	li $a3, 24
+	li $a3, 32
 	jal DrawHorizontalLine
+	li $a0, 30
+	lw $a2, backgroundColour
+	jal DrawPoint
+	li $a0, 28
 	li $a1, 6
 	lw $a2, backgroundColour
 	jal DrawPoint
 	
-	li $a0, 12
+	li $a0, 20
 	li $a1, 6
 	jal DrawPoint
-	li $a0, 28
+	li $a0, 36
 	jal DrawPoint
 	li $a1, 2
 	jal DrawPoint
 	
-	addi $t0, $t0, 1
-	sw $t0, notifNum 
-				
-#udontsuck:
-#pog:	
-
-
+	addi $s7, $s7, 1
+	sw $s7, notifNum 
 	
-leave4:	
 	addi $s6, $s6, 5
 	sw $s6, notifThreshold
 	
+	j leave4
+	
+pog:	
+	li $a0, 15
+	li $a1, 2
+	lw $a2, orange
+	li $a3, 25
+	jal DrawHorizontalLine
+	li $a0, 18
+	lw $a2, backgroundColour
+	jal DrawPoint
+	li $a0, 22
+	jal DrawPoint
+	
+	li $a0, 15
+	li $a1, 2
+	lw $a2, orange
+	li $a3, 6
+	jal DrawVerticalLine
+	li $a0, 19
+	jal DrawVerticalLine
+	li $a0, 21
+	jal DrawVerticalLine
+	li $a0, 23
+	jal DrawVerticalLine
+	
+	li $a0, 19
+	li $a1, 6
+	lw $a2, orange
+	li $a3, 25
+	jal DrawHorizontalLine
+	li $a0, 22
+	lw $a2, backgroundColour
+	jal DrawPoint
+	
+	li $a0, 17
+	li $a1, 2
+	lw $a2, orange
+	li $a3, 4
+	jal DrawVerticalLine
+	li $a0, 25
+	li $a1, 4
+	li $a3, 6
+	jal DrawVerticalLine
+	li $a0, 16
+	jal DrawPoint
+	li $a0, 24
+	jal DrawPoint
+####display######	
+	li $v0 32
+	li $a0, 500
+	syscall
+####display######		
+	li $a0, 15
+	li $a1, 2
+	lw $a2, backgroundColour
+	li $a3, 25
+	jal DrawHorizontalLine
+	li $a0, 18
+	lw $a2, backgroundColour
+	jal DrawPoint
+	li $a0, 22
+	jal DrawPoint
+	
+	li $a0, 15
+	li $a1, 2
+	lw $a2, backgroundColour
+	li $a3, 6
+	jal DrawVerticalLine
+	li $a0, 19
+	jal DrawVerticalLine
+	li $a0, 21
+	jal DrawVerticalLine
+	li $a0, 23
+	jal DrawVerticalLine
+	
+	li $a0, 19
+	li $a1, 6
+	lw $a2, backgroundColour
+	li $a3, 25
+	jal DrawHorizontalLine
+	li $a0, 22
+	lw $a2, backgroundColour
+	jal DrawPoint
+	
+	li $a0, 17
+	li $a1, 2
+	lw $a2, backgroundColour
+	li $a3, 4
+	jal DrawVerticalLine
+	li $a0, 25
+	li $a1, 4
+	li $a3, 6
+	jal DrawVerticalLine
+	li $a0, 16
+	jal DrawPoint
+	li $a0, 24
+	jal DrawPoint
+	
+	li $s7, 0
+	sw $s7, notifNum 
+	
+	addi $s6, $s6, 5
+	sw $s6, notifThreshold
+	
+leave4:	
 	
 	lw $ra, 0($sp)
    	addi $sp, $sp, 4
    	
    	jr $ra
+##################################################################
+# Draw score
+#################################################################
+drawScore:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	li $a0, 1
+	li $a1, 1
+	lw $a2, backgroundColour
+	li $a3, 5
+	jal DrawVerticalLine
+	li $a0, 2
+	jal DrawVerticalLine
+	li $a0, 3
+	jal DrawVerticalLine
+	li $a0, 5
+	jal DrawVerticalLine
+	li $a0, 6
+	jal DrawVerticalLine
+	li $a0, 7
+	jal DrawVerticalLine
+	
+	lw $a0, score
+
+	beqz $a0, zero1
+	beq $a0, 1, one1
+	beq $a0, 2, two1
+	beq $a0, 3, three1
+	beq $a0, 4, four1
+	beq $a0, 5, five1
+	beq $a0, 6, six1
+	beq $a0, 7, seven1
+	beq $a0, 8, eight1
+	bge $a0, 9, nine1
+
+	j leave1
+zero1:
+	li $a0, 1
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 5
+	jal DrawVerticalLine
+	li $a0, 3
+	jal DrawVerticalLine
+	li $a0, 2
+	li $a1, 1
+	jal DrawPoint
+	li $a1, 5
+	jal DrawPoint
+
+	
+	j digit2
+one1:	
+	li $a0, 3
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 5
+	jal DrawVerticalLine
+	
+	j digit2
+	
+two1:	
+	li $a0, 1
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 3
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	li $a1, 5
+	jal DrawHorizontalLine
+	
+	li $a0, 3
+	li $a1, 2
+	jal DrawPoint
+	li $a0, 1
+	li $a1, 4
+	jal DrawPoint
+	
+	j digit2
+three1:
+		
+	li $a0, 1
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 3
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	li $a1, 5
+	jal DrawHorizontalLine	
+
+	li $a0, 3
+	li $a1, 2
+	jal DrawPoint
+	li $a0, 3
+	li $a1, 4
+	jal DrawPoint
+	
+	j digit2
+	
+four1:
+	li $a0, 1
+	li $a1, 3
+	lw $a2, scoreCol
+	li $a3, 3
+	jal DrawHorizontalLine
+	li $a1, 1
+	jal DrawVerticalLine
+	li $a0, 3
+	li $a3, 5
+	jal DrawVerticalLine
+	
+	j digit2
+five1:
+	li $a0, 1
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 3
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	li $a1, 5
+	jal DrawHorizontalLine	
+	
+	li $a0, 1
+	li $a1, 2
+	jal DrawPoint
+	li $a0, 3
+	li $a1, 4
+	jal DrawPoint
+	
+	j digit2
+six1:
+	li $a0, 1
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 3
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	li $a1, 5
+	jal DrawHorizontalLine	
+	li $a0, 1
+	li $a1, 2
+	jal DrawPoint
+	li $a1, 4
+	jal DrawPoint
+	li $a0, 3
+	li $a1, 4
+	jal DrawPoint
+	j digit2
+seven1:
+	li $a0, 1
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 3
+	jal DrawHorizontalLine
+	li $a0, 3
+	li $a1, 1
+	li $a3, 5
+	jal DrawVerticalLine
+	j digit2
+eight1:
+	li $a0, 1
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 3
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	li $a1, 5
+	jal DrawHorizontalLine	
+	
+	li $a0, 1
+	li $a1, 1
+	li $a3, 5
+	jal DrawVerticalLine
+	li $a0, 3
+	jal DrawVerticalLine
+	
+	j digit2
+nine1:
+	
+	li $a0, 1
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 3
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	
+	li $a0, 3
+	li $a1, 1
+	li $a3, 5
+	jal DrawVerticalLine
+	
+	li $a0, 1
+	li $a1, 2
+	jal DrawPoint 
+	
+	j digit2
+
+digit2:
+	lw $a0, score2
+	beqz $a0, zero2
+	beq $a0, 1, one2
+	beq $a0, 2, two2
+	beq $a0, 3, three2
+	beq $a0, 4, four2
+	beq $a0, 5, five2
+	beq $a0, 6, six2
+	beq $a0, 7, seven2
+	beq $a0, 8, eight2
+	bge $a0, 9, nine2
+zero2:
+	li $a0, 5
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 5
+	jal DrawVerticalLine
+	li $a0, 7
+	jal DrawVerticalLine
+	li $a0, 6
+	li $a1, 1
+	jal DrawPoint
+	li $a1, 5
+	jal DrawPoint
+	
+	j leave1
+one2:
+	li $a0, 7
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 5
+	jal DrawVerticalLine
+	
+	j leave1
+two2:	
+	li $a0, 5
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 7
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	li $a1, 5
+	jal DrawHorizontalLine
+	
+	li $a0, 7
+	li $a1, 2
+	jal DrawPoint
+	li $a0, 5
+	li $a1, 4
+	jal DrawPoint
+	
+	j leave1
+three2:
+		
+	li $a0, 5
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 7
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	li $a1, 5
+	jal DrawHorizontalLine	
+
+	li $a0, 7
+	li $a1, 2
+	jal DrawPoint
+	li $a0, 7
+	li $a1, 4
+	jal DrawPoint
+	
+	j leave1
+four2:
+	li $a0, 5
+	li $a1, 3
+	lw $a2, scoreCol
+	li $a3, 7
+	jal DrawHorizontalLine
+	li $a1, 1
+	li $a3, 3
+	jal DrawVerticalLine
+	li $a0, 7
+	li $a3, 5
+	jal DrawVerticalLine
+	
+	j leave1
+five2:
+	li $a0, 5
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 7
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	li $a1, 5
+	jal DrawHorizontalLine	
+	
+	li $a0, 5
+	li $a1, 2
+	jal DrawPoint
+	li $a0, 7
+	li $a1, 4
+	jal DrawPoint
+	
+	j leave1
+six2:
+	li $a0, 5
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 7
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	li $a1, 5
+	jal DrawHorizontalLine	
+	li $a0, 5
+	li $a1, 2
+	jal DrawPoint
+	li $a1, 4
+	jal DrawPoint
+	li $a0, 7
+	li $a1, 4
+	jal DrawPoint
+	j leave1
+seven2:
+	li $a0, 5
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 7
+	jal DrawHorizontalLine
+	li $a0, 7
+	li $a1, 1
+	li $a3, 5
+	jal DrawVerticalLine
+	j leave1
+eight2:
+	li $a0, 5
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 7
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	li $a1, 5
+	jal DrawHorizontalLine	
+	
+	li $a0, 5
+	li $a1, 1
+	li $a3, 5
+	jal DrawVerticalLine
+	li $a0, 7
+	jal DrawVerticalLine
+	
+	j leave1
+nine2:
+	
+	li $a0, 5
+	li $a1, 1
+	lw $a2, scoreCol
+	li $a3, 7
+	jal DrawHorizontalLine
+	li $a1, 3
+	jal DrawHorizontalLine
+	
+	li $a0, 7
+	li $a1, 1
+	li $a3, 5
+	jal DrawVerticalLine
+	
+	li $a0, 5
+	li $a1, 2
+	jal DrawPoint 
+	
+	j leave1
+leave1:
+	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+
+	jr $ra
+
 ##################################################################
 # ended
 #################################################################
